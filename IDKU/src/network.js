6e4b5ab6e0c79d46e95a98b6ebea5266f6ddd343
secondBattalionCommander <1050552217@qq.com>
2018-09-23 12:48:39 +0800

    想商家付款增加选择课程

--- IDKU/src/network.js
+++ IDKU/src/network.js
@@ -1,7 +1,7 @@
 const apiTable = {
   '': {
     type: 2,
-    protocol: 'https://',
+    protocol: 'http',
     // ip: '192.168.0.86',
     ip: '218.108.32.142',
     dev_domain: 'xuebei.atzhixiao.com',
@@ -11,7 +11,7 @@ const apiTable = {
   },
   'xb/': {
     type: 2,
-    protocol: 'https://',
+    protocol: 'http',
     // ip: '192.168.0.86',
     ip: '218.108.32.142',
     dev_domain: 'xuebei.atzhixiao.com',
@@ -36,11 +36,11 @@ function getBaseUrl(api) {
   }
   switch (table.type) {
     case 0:// ip
-      return table.protocol + table.ip + table.port + table.project
+      return table.protocol +"://"+ table.ip + table.port + table.project
     case 1:// dev_domain
-      return table.protocol + table.dev_domain + table.project
+      return table.protocol +"://"+ table.dev_domain + table.project
     case 2:// on-line
-      return table.protocol + table.domain + table.project
+      return table.protocol+'s'+"://" + table.domain + table.project
   }
 }
 
